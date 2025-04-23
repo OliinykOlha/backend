eef6c4927f6cd10de3e4055dc6fd33efc5cb2a29
OlhaOliinyk <olaolijnik71@gmail.com>
2025-04-23 17:54:15 +0200

    Account synchronized, transfer

---
+++ lessons/lesson_05/demo20250423_2/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static Object lock = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        Account accountA = new Account("DE1111", "Jack", 1000);
+        Account accountB = new Account("DE2222", "John", 1000);
+
+        Thread thread1 = new Thread(() -> transfer(accountA, accountB, 100), "T1");
+        Thread thread2 = new Thread(() -> transfer(accountB, accountA, 500), "T2");
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(accountA);
+        System.out.println(accountB);
+
+    }
+
+    public static void transfer(Account from, Account to, double amount) {
+        Account first = from.hashCode() < to.hashCode() ? from : to;
+        Account second =  from.hashCode() < to.hashCode() ? to : from;
+
+
+        synchronized (first) {
+            System.out.println("account" + from + " is locked " + Thread.currentThread().getName());
+
+            synchronized (second) {
+                System.out.println("account" + to + " is locked" + Thread.currentThread().getName());
+
+                from.withdraw(amount);
+                to.deposit(amount);
+            }
+            System.out.println("account" + to + " is unlocked" + Thread.currentThread().getName());
+        }
+        System.out.println("account" + from + " is unlocked" + Thread.currentThread().getName());
+        System.out.println("transfer is finish");
+    }
+}
