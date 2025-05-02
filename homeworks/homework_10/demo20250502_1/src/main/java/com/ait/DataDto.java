e92fb427f28c6f7431a32579959ecb179fc2400e
OlhaOliinyk <olaolijnik71@gmail.com>
2025-05-02 11:09:10 +0200

    homework 10

---
+++ homeworks/homework_10/demo20250502_1/src/main/java/com/ait/DataDto.java
@@ -0,0 +1,14 @@
+package com.ait;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+public class DataDto {
+    private String data;
+}
