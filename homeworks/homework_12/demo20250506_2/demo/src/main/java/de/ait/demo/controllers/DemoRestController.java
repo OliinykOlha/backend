9a4a2bbfbcf3911450f2040d0175bebc708e893e
OlhaOliinyk <olaolijnik71@gmail.com>
2025-05-06 20:21:23 +0200

    homework 12 added DateDto

--- homeworks/homework_12/demo20250506_2/demo/src/main/java/de/ait/demo/controllers/DemoRestController.java
+++ homeworks/homework_12/demo20250506_2/demo/src/main/java/de/ait/demo/controllers/DemoRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @RestController
@@ -14,12 +15,12 @@ public class DemoRestController {
 
     @RequestMapping(value = "/books", method = RequestMethod.GET)
     public List<Book> getBook() {
-     List<Book> books = new ArrayList<>();
-     books.add(new Book("Curious George", "H.A.Rey", 2000, "235981" ));
-     books.add(new Book("Harry Potter", "J.K.Rowling", 2000, "236458" ));
-     books.add(new Book("Holes", "C.S.Lewis", 2016, "226409" ));
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Curious George", "H.A.Rey", 2000, "235981"));
+        books.add(new Book("Harry Potter", "J.K.Rowling", 2000, "236458"));
+        books.add(new Book("Holes", "C.S.Lewis", 2016, "226409"));
 
-     return books;
+        return books;
     }
 
     @RequestMapping(value = "/films", method = RequestMethod.GET)
@@ -32,10 +33,17 @@ public class DemoRestController {
         return films;
     }
 
-@RequestMapping(value = "/now", method = RequestMethod.GET)
-    public List<String> getCurrentTime() {
-        LocalDateTime now = LocalDateTime.now();
-    String formattedTime = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-    return List.of(formattedTime);
+
+    @RequestMapping(value = "/now", method = RequestMethod.GET)
+    public DateDto getCurrentTime() {
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int year = calendar.get(Calendar.YEAR);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+
+        return new DateDto(day, month, year, hour, minute, second);
     }
 }
